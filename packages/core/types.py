4c2f8a2192b6b26597e6a55c51ca47c61c95c9b2
Sanjay G <gokulesh2615@gmail.com>
2026-07-06 15:44:49 +0530

    feat(types): define domain type system and data contracts

---
+++ packages/core/types.py
@@ -0,0 +1,22 @@
+from typing import Dict, List
+
+class KnowledgeGraph:
+    def __init__(self, nodes: List[Dict] = None):
+        self.nodes = nodes if nodes is not None else []
+
+    def get_nodes(self, target: str) -> List[Dict]:
+        # Get nodes from the knowledge graph that match the target
+        nodes = [node for node in self.nodes if node['target'] == target]
+        return nodes
+
+class ThreatModelDB:
+    def __init__(self, threat_models: Dict = None):
+        self.threat_models = threat_models if threat_models is not None else {}
+
+    def get_effectiveness(self, action: str, target: str) -> float:
+        # Get the effectiveness of the action on the target from the threat model database
+        threat_model = self.threat_models.get((action, target))
+        if threat_model:
+            return threat_model['effectiveness']
+        else:
+            return 0.0
